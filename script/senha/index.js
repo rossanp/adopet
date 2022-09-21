e094bb137fd8e6ce3fcdbc1ecfdc3bc9541747b5
Rossan P <rossan.p23@gmail.com>
2022-09-20 23:34:55 -0300

    Criação ação mostrar e ocultar senha

---
+++ script/senha/index.js
@@ -0,0 +1,9 @@
+console.clear()
+
+var senha = document.getElementById('senha')
+var olho = document.getElementById('form__inpt-olho')
+
+olho.addEventListener( 'click', function() {
+    
+})
+console.log(senha)
