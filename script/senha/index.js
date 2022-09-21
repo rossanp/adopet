89c9ffc587e78445bebe25d5f6344fddf16fda68
rossanp <rossan.p23@gmail.com>
2022-09-21 11:27:49 -0300

    Criando interação com cadastro de senhas

--- script/senha/index.js
+++ script/senha/index.js
@@ -1,9 +1,35 @@
-console.clear()
+var mostrarSenha = document.getElementById('form__inpt-olho')
+var mostrarConfSenha = document.getElementById('form__inpt-conf-olho')
+var cadastrar = document.getElementById('btn-cadastrar')
+
+mostrarSenha.addEventListener( 'click', function() {
+    var senha = document.getElementById('senha')
+    
+    if ( senha.type === "password" ) {
+        senha.type = "text"
+    } else {
+        senha.type = "password"
+    }
+});
+
+mostrarConfSenha.addEventListener( 'click', function() {
+    var confSenha = document.getElementById('confirma-senha')
+    
+    if ( confSenha.type === "password" ) {
+        confSenha.type = "text"
+    } else {
+        confSenha.type = "password"
+    }
+})
 
 var senha = document.getElementById('senha')
-var olho = document.getElementById('form__inpt-olho')
+var confSenha = document.getElementById('confirma-senha')
+var senhaIncorreta = document.getElementById('senha-incorreta')
 
-olho.addEventListener( 'click', function() {
-    
+cadastrar.addEventListener( 'click', function() {
+    if ( senha && confSenha === "" || "undefinded") {
+        if ( confSenha.value != senha.value ) {
+            senhaIncorreta.innerHTML = "As senhas não são as mesmas"
+        }
+    }
 })
-console.log(senha)
